7378efff42b1e6ae7824fc9f8030d467f30fbad4
Howard Schneider <howard.schneider@gmail.com>
2018-09-03 10:29:42 -0400

    Add files via upload

---
+++ hello_world.py
@@ -0,0 +1,15 @@
+#
+# test code for sandbox
+# test code to include in all repositories so that
+# automated code verification software will not
+# give failure results for the repository.
+#
+#
+print ("hello world")
+print ("\nThis Python file is included in all repositorie so that")
+print ("automated code verification software can go through all")
+print ("directories without giving failure analysis reports for those")
+print ("directories, possibly such as this one, which at this time is")
+print ("not actively used for code development but may just be holding")
+print ("files from presentations, files to share, etc.\n")
+

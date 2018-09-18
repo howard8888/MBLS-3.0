bf093eec4477855b58b5f1366d06540178ae4b42
Howard Schneider <howard.schneider@gmail.com>
2018-09-17 21:53:14 -0400

    Add files via upload

--- hello_world.py
+++ hello_world.py
@@ -1,15 +1,11 @@
 #
-# test code for sandbox
-# test code to include in all repositories so that
-# automated code verification software will not
-# give failure results for the repository.
 #
+#Test Code for Sandbox
 #
-print ("hello world")
-print ("\nThis Python file is included in all repositorie so that")
-print ("automated code verification software can go through all")
-print ("directories without giving failure analysis reports for those")
-print ("directories, possibly such as this one, which at this time is")
-print ("not actively used for code development but may just be holding")
-print ("files from presentations, files to share, etc.\n")
+'''Test code now included in all repositories so that automated code verification
+software will not give failure results for the repository.
+Pylint should give 10/10 for each automated run. (Please run Pylint manually on actual code.)
+'''
 
+print("hello world")
+print("\nThis Python file is included in all repositories.")
